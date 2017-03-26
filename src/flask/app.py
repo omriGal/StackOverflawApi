2ee2556672dc11e3fa71e030dbd6baf6de678c49
omriGal <gal.omri@gmail.com>
2017-03-26 14:45:07 -0700

    Get my posts - dynamic

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 
 from src.flask.post_by_user import get_post_by_id
 
-app = Flask(__name__ ,template_folder='../templates')
+app = Flask(__name__, template_folder='../templates')
+app.config['SECRET_KEY'] = 'F34TF$($e34D';
 
 @app.route("/")
-def main():
+def home():
     return render_template('home.html')
 
-@app.route('/myPosts')
-def myPosts():
-    # return render_template('myPosts.html')
-    return get_post_by_id()
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/myPosts', methods=['GET', 'POST'])
+def my_posts():
+    session['user_id'] = request.form['user_id']
+    print session['user_id']
+    return get_post_by_id(session['user_id'])
+
+
+if __name__ == "__main__":    app.run()
--- src/flask/post_by_user.py
+++ src/flask/post_by_user.py
@@ -1,42 +1,51 @@
 import requests
 import json
 
+SUFFIX_RESPONSE_POST = "</ul></body> </html>"
+
+PREFIX_RESPONSE_POST = "<html> " \
+                       "<body> " \
+                       "<h1>Stack Overflow posts by user_id:</h1>" \
+                       "<ul style='list-style-type:circle'>"
+
 STACKOVERFLOW_URL = "http://api.stackexchange.com/2.2/users/"
 POSTS_URL_SUFFIX = "/posts"
 
-def get_post_by_id():
 
-    user_id = "3729523"
+def get_post_by_id(user_id):
+
     full_url = STACKOVERFLOW_URL + user_id + POSTS_URL_SUFFIX
 
-    #debug
+    # debug
     print full_url
 
     param = {"site": "stackoverflow"}
     g = requests.get(full_url, params=param)
 
-    #debug
+    # debug
     print g.url
     print g.status_code
     print g.text
 
     json_response = json.loads(g.text)
 
-    #debug
+    # debug
     print json_response
     print len(json_response["items"])
 
-    my_response = "<html> " \
-                  "<body> "
+    user_name = json_response["items"][0]['owner']['display_name']
+    my_response = PREFIX_RESPONSE_POST
+    my_response += '<h2> Links to ' + user_name + ' posts </h2>'
+
     for item in json_response["items"]:
-        my_response += '<a href="' + item["link"] + '">' + item["link"] + '</a>'
-    my_response += "</body> </html>"
+        my_response += '<li><a href="' + item["link"] + '">' + item["link"] + '</a></li> <br>'
+
+        #debug
+        print my_response
+
+    if json_response["has_more"]: my_response += '<h3> User has more posts</h3>'
+    my_response += SUFFIX_RESPONSE_POST
 
     return my_response
-
-
-get_post_by_id()
-
-
 
 
