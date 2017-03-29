ce9129968cafc124ad0a16f8f529f433e15cd0f0
omriGal <gal.omri@gmail.com>
2017-03-29 09:36:58 -0700

    before submit

--- src/flask/app.py
+++ src/flask/app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, request, session
 from flask import url_for
 from flask_oauth import OAuth
 
-from src.flask.utils.post_by_user import get_post_by_id
+from src.flask.post_by_user import get_post_by_id
 
-OAUTH_DIALOG_URL = 'https://stackexchange.com/oauth/dialog'
 CLIENT_ID = '9285'
 CLIENT_SECRET = 'k*EAoyC995EOOW*Tv0i3Bg(('
-REDIRECT_URI = 'https://stackexchange.com/oauth/login_success'
 SCOPE = 'no_expiry'
 
 app = Flask(__name__, template_folder='../templates')
@@ -15,7 +13,7 @@ app.config['SECRET_KEY'] = 'F34TF$($e34D';
 
 oauth = OAuth()
 stackExchange = oauth.remote_app('stackexchange',
-                                 base_url='https://api.stackexchange.com/2.2',
+                                 base_url=None,
                                  request_token_url=None,
                                  access_token_url='/oauth/access_token',
                                  authorize_url='https://stackexchange.com/oauth/dialog',
@@ -39,7 +37,7 @@ def get_user_posts():
 @app.route('/auth', methods=['GET'])
 def get_auth():
     return stackExchange.authorize(callback=url_for('stackExchange_authorized',
-                                                    next=request.args.get('next') or request.referrer or None, _external=True))
+                                                    next=request.args.get('next') or None, _external=True))
 
 
 @app.route('/login/authorized')
@@ -54,6 +52,7 @@ def stackExchange_authorized(response):
     session['access_token'] = (response['access_token'], '')
     me = stackExchange.get('me/posts')
     return render_template('myPosts.html', user_id="By-auth", posts=me, more=me)
+
 
 @stackExchange.tokengetter
 def get_stackExchange_oauth_token():
--- src/flask/post_by_user.py
+++ src/flask/post_by_user.py
@@ -23,20 +23,3 @@ def get_post_by_id(user_id):
         return user_posts, flag
     return None
 
-# def get_post_by_access_token():
-#     full_url = STACKOVERFLOW_URL + "me/posts" + POSTS_URL_SUFFIX
-#
-#     param = {"site": "stackoverflow"}
-#     response = requests.get(full_url, params=param)
-#     if response:
-#         json_response = json.loads(response.text)
-#
-#         user_posts = []
-#         for item in json_response["items"]:
-#             user_posts.append(item["link"])
-#
-#         if json_response["has_more"]: flag = True
-#         else: flag = False
-#
-#         return user_posts, flag
-#     return None
